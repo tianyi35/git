ac393983291763257fc853cebfe77e65ea725684
Lxp <lxp@qq.com>
2023-04-25 12:13:47 +0800

    first commit

---
+++ src/main/java/com/agg/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.agg.git;
+
+/**
+ * Description: <描述>
+ * Author: Lixp
+ * Date: 2023-04-25 12:11
+ * Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+    }
+}
